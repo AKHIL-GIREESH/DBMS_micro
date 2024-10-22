e1ff2dcb7969573e5bf4df27c41b67244221fee0
Akhil Gireesh <akhilgireesh@Akhils-MacBook-Air-2.local>
2024-10-22 12:50:07 +0530

    boilerplate

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
 import App from './App.tsx'
 import './index.css'
+import Login from "../pages/Login/Login.tsx"
+import SignUp from "../pages/SignUp/SignUp.tsx"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path:"/login",
+    element:<Login/>
+  }, 
+  {
+    path:"/signup",
+    element:<SignUp/>
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
