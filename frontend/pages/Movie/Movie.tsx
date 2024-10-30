8ffdf850240f81bddd5853e740ed93ed3523e433
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-10-30 18:28:21 +0530

    frontend movie layout

--- frontend/pages/Movie/Movie.tsx
+++ frontend/pages/Movie/Movie.tsx
@@ -5,19 +5,41 @@ import { Button } from "@/components/ui/button"
 const Movie = () => {
     const {id} = useParams()
     return(
-        <div className="h-[100vh] w-[100vw]">
-            <div className="h-[40vh] w-[100%]  border border-black flex">
+        <div className=" text-center ml-12 mr-12">
+            <div className="h-[50vh] w-[100%] ml-6 border border-black flex">
                 <img src={Titanic} className="h-[100%]"/>
-                <div>
-                    <h1 className="">MOVIE {id}</h1>
+                <div className="mt-[5vh] border border-black ml-10">
+                    <h1 className="font-extrabold">MOVIE {id}</h1>
                     <p>6.9<b>·</b>10</p>
+                    <Button>Book Tickets</Button>
                 </div>
             </div>
-            <Button>Book Tickets</Button>
-            <div>
-                <h1>
+            <br/>
+            <div className="flex flex-col items-center justify-center border border-black">
+                <h1 className="font-extrabold">
                     TRAILER
                 </h1>
+                <br/>
+                
+                <iframe
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/watch?v=wjZofJX0v4M`}
+                allowFullScreen
+                title="YouTube video"
+                ></iframe>
+                <br/>
+                <br/>
+                <p className="font-medium">Lorem ipsum dolor sit amet. In nulla dolorem quo beatae quasi in rerum sapiente hic eligendi facilis qui amet ipsam et molestiae distinctio! Eum quia assumenda qui numquam debitis sed perferendis quas quo expedita consequatur eos quia asperiores.
+
+Eum veniam natus et ipsum officiis vel accusamus tenetur eum odio delectus? Aut repellendus quia et officia voluptatem est eveniet laborum. Et dolore possimus qui officiis Quis aut omnis facilis aut cumque tempore aut quam architecto et eaque blanditiis hic dolores vitae.</p>
+            </div>
+            <br/>
+            <div>
+                <h1 className="font-extrabold">
+                    REVIEWS
+                </h1>
+                <br/>
             </div>
         </div>
     )
